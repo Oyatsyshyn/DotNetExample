c15770491dbe7fcd7dfcf4ae5b752199f3df6fbd
Oyatsyshyn <sashayatsyshyn72@users.noreply.github.com>
2020-03-02 10:02:29 +0200

    edit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static public int Example(int a)
+        {
+            int c = a*a*a;
+            Console.WriteLine(c.ToString());
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введiть довжину ребра");
+            string S1 = Console.ReadLine();
+            int a = int.Parse(S1);
+            
+            Example(a);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp2Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp2Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int a = 4;
+            int rezult =
+        }
+    }
+}
